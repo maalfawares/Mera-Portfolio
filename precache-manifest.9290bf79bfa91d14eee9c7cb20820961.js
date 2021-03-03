4aa1c3c2606095bcc96678eb261706748a407073
Mera Alfawares <maalfawares@bsu.edu>
2021-03-03 16:33:30 -0500

    Updates

---
+++ precache-manifest.9290bf79bfa91d14eee9c7cb20820961.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a370424e57ce2eb912921297a332884c",
+    "url": "/mera-portfolio/index.html"
+  },
+  {
+    "revision": "bf023ed83fe50aac74ec",
+    "url": "/mera-portfolio/static/css/2.75922eb6.chunk.css"
+  },
+  {
+    "revision": "7aa778dc54203e957585",
+    "url": "/mera-portfolio/static/css/main.4458e121.chunk.css"
+  },
+  {
+    "revision": "bf023ed83fe50aac74ec",
+    "url": "/mera-portfolio/static/js/2.980723d5.chunk.js"
+  },
+  {
+    "revision": "e518d035a71c6847b18a0a62019e88bf",
+    "url": "/mera-portfolio/static/js/2.980723d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa778dc54203e957585",
+    "url": "/mera-portfolio/static/js/main.1208fb77.chunk.js"
+  },
+  {
+    "revision": "5a6b71f8906b6bb13fec",
+    "url": "/mera-portfolio/static/js/runtime-main.1e1a2fee.js"
+  },
+  {
+    "revision": "2efaa1d902538a4f6e3cc3be9fd73d21",
+    "url": "/mera-portfolio/static/media/EVELogo.2efaa1d9.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/mera-portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/mera-portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/mera-portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/mera-portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/mera-portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "87ffdcdd3aef047dac4a4c61a9a113e1",
+    "url": "/mera-portfolio/static/media/email.87ffdcdd.svg"
+  },
+  {
+    "revision": "a7ad083da81b94284857f1f0475efdae",
+    "url": "/mera-portfolio/static/media/file.a7ad083d.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/mera-portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "f77e8b325127a176a7ac0211370cc6dd",
+    "url": "/mera-portfolio/static/media/github.f77e8b32.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/mera-portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/mera-portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/mera-portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/mera-portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/mera-portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "aaf16c7550f3ea221ef99f3bcbe57b4b",
+    "url": "/mera-portfolio/static/media/landing_pic.aaf16c75.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/mera-portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/mera-portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/mera-portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/mera-portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/mera-portfolio/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
